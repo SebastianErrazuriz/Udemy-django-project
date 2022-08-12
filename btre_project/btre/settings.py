5fb4448372685c3bf23f6397241bc51245523004
Sebastián Errázuriz <s.errazuriz93@gmail.com>
2022-08-12 11:56:32 -0400

    settings btre

--- btre_project/btre/settings.py
+++ btre_project/btre/settings.py
@@ -139,3 +139,10 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.INFO: '',
+    50: 'critical',
+}
