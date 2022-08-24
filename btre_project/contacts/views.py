4f6571db82434a54c583bfbd4b9c343101c40689
Sebastián Errázuriz <s.errazuriz93@gmail.com>
2022-08-24 11:17:13 -0400

    Inquery Check and Send Email

--- btre_project/contacts/views.py
+++ btre_project/contacts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
+
 
 def contact(request):
     if request.method == 'POST':
@@ -13,6 +15,14 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        # Chek if user has made inquery already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made an inquery for this listing')
+                return redirect('/listings/' + listing_id)
+
         contact = Contact(listing=listing,
                           listing_id=listing_id,
                           name=name,
@@ -21,6 +31,15 @@ def contact(request):
                           message=message,
                           user_id=user_id)
         contact.save()
+
+        # Send email
+        send_mail(
+            'Property Listing Inquery',
+            'There has been an inquery for ' + listing + '. Sign into the admin panel for more info',
+            's.errazuriz93@gmail.com',
+            [realtor_email, 's.errazuriz93@gmail.com'],
+            fail_silently=False
+        )
 
         messages.success(request, 'Your request have been submitted, a realtor will get back to you son')
         return redirect('/listings/'+listing_id)
